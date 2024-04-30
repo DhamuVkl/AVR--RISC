b7312af41ed1878fbf8aefb8f0e75b346de04685
Dharagesh <sdhamuvkl@gmail.com>
2024-04-30 18:07:32 +0530

    #

--- interrupt/main.c
+++ interrupt/main.c
@@ -3,28 +3,29 @@
 
 // goal is to flip LED every second;
 #define F_CPU 16000000
-ISR (TIMER1_OVF_vect)
+ISR(TIMER1_OVF_vect)
 {
-    PORTB ^= _BV(PORTB5);
-    TCNT1 = 65535 - (F_CPU / 1024);
+  PORTB ^= _BV(PORTB5);
+  TCNT1 = 65535 - (F_CPU / 1024);
 }
 int main(void)
 {
 
-    DDRB |= _BV(DDB5);
+  DDRB |= _BV(DDB5);
 
-    // this has a max value of 65535
-    TCNT1 = 65535 - (F_CPU / 1024);
-    // sets the prescaler to 1024
-    // 16M/1024 = 15625Hz
-    TCCR1B = (1 << CS10) | (1 << CS12);
-    TCCR1A = 0X00;
-    TIMSK1 = (1 << TOIE1);
+  // this has a max value of 65535
+  TCNT1 = 65535 - (F_CPU / 1024);
+  // sets the prescaler to 1024
+  // 16M/1024 = 15625Hz
+  TCCR1B = (1 << CS10) | (1 << CS12);
+  TCCR1A = 0X00;
+  TIMSK1 = (1 << TOIE1);
 
-    sei();
+  sei();
 
-    while (1)
-    {
-      ;; 
-    }
+  while (1)
+  {
+    ;
+    ;
+  }
 }
